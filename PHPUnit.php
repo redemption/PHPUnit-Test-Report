b25400bee537f72e227092913b01b0559e6b0b61
redemption <bluespire@gmail.com>
2010-12-09 20:03:37 -0800

    Initial work to get PHPUnit Test Report to work with PHPUnit 3.5.x.
    The download version from Matthew's Git repository doesn't work with PHPUnit 3.5.x because it uses the old Framework.php include instead of the newer Autoload.php file.
    
    Also, I added a constant to control what file suffix to look for and changed it to default to Test.php, which matches more with the docs for 3.5.x for some reason there is never any output from my Test files

--- PHPUnit.php
+++ PHPUnit.php
@@ -1,6 +1,6 @@
 <?php
 
-/** 
+/**
 * Short Description
 *
 * Long Description
@@ -8,18 +8,24 @@
 * @author Matt Mueller
 */
 
+define('PHPUnit_VERSION','3.5.5');
+define('TEST_CASE_SUFFIX','Test.php');// default is 'TestCase.php', but we use Test.php to match with PHPUnit 3.5.x's new standards.
 
-set_include_path(dirname(__FILE__));
-require_once('PHPUnit/Framework.php');
+if (PHPUnit_VERSION >= 3.5) {
+	require_once('PHPUnit/Autoload.php');
+} else { //don't set include path
+	set_include_path(dirname(__FILE__));
+	require_once('PHPUnit/Framework.php');
+}
 
 
-class PHPUnit 
+class PHPUnit
 {
 	public $testDir;
 	public $testCases = array();
 	public $results;
 	public $suite;
-	
+
 	function __construct($testDir, $suite = '')
 	{
 		if(is_dir(realpath($testDir))) {
@@ -27,26 +33,26 @@ class PHPUnit
 		} else {
 			throw new Exception("$testDir is not a dir", 1);
 		}
-		
+
 		$path = realpath($this->testDir);
 		$this->suite = $suite;
 
 		$objects = new RecursiveIteratorIterator(
-					new RecursiveDirectoryIterator($path), 
+					new RecursiveDirectoryIterator($path),
 					RecursiveIteratorIterator::SELF_FIRST
-				   );
-				
+					);
+
 		foreach($objects as $name){
-			if(strstr($name, 'TestCase.php') !== false) {
+			if(strstr($name, TEST_CASE_SUFFIX) !== false) {
 				$parts = explode('/', $name);
 				$filename = end($parts);
-				$filename = strtolower(str_replace('TestCase.php', '', $filename));
+				$filename = strtolower(str_replace(TEST_CASE_SUFFIX, '', $filename));
 				$this->testCases[$filename] = (string) $name;
 			}
 		}
-				
+
 	}
-	
+
 	public function run($name = null) {
 		$tests = array();
 		if(!isset($name)) {
@@ -63,24 +69,23 @@ class PHPUnit
 			if(is_numeric($name)) {
 				$name = $mixed;
 			}
-			
+
 			$name = strtolower($name);
 			$path = $this->testCases[$name];
-
 			require_once($path);
 		}
 
 		$testClasses = array();
-		
+
 		foreach(get_declared_classes() as $class) {
-			if((stristr($class, 'Test') !== false) && (strpos($class, 'PHPUnit_') === false)) {
+			if(preg_match("/Test$/",$class) && (strpos($class, 'PHPUnit_') === false)) {
 				$testClasses[] = $class;
 			}
 		}
 
 		$suite = new PHPUnit_Framework_TestSuite();
-		
-		
+
+
 		foreach ($testClasses as $class) {
 			if($this->suite) {
 				if(strcasecmp($this->suite, $class) == 0) {
@@ -89,16 +94,16 @@ class PHPUnit
 			} else {
 				$suite->addTestSuite($class);
 			}
-			
+
 		}
-		
+
 		$result = new PHPUnit_Framework_TestResult;
 		require_once 'PHPUnit/Util/Log/JSON.php';
 
-        $result->addListener(
-          new PHPUnit_Util_Log_JSON
-        );
-		
+		  $result->addListener(
+			 new PHPUnit_Util_Log_JSON
+		  );
+
 		$this->suite = $suite;
 
 
@@ -106,18 +111,18 @@ class PHPUnit
 		$suite->run($result);
 		$results = ob_get_contents();
 		ob_end_clean();
-		
+
 		return $results;
 
 	}
-	
+
 	public static function toHTML($result) {
 
 		$results = array();
 		$collectedData = $result;
-		
+
 		$jsonElements = self::pull($result);
-		
+
 		foreach ($jsonElements as $elem) {
 			$elem = '{'.$elem.'}';
 			$collectedData = self::push($elem, '|||', $collectedData);
@@ -125,31 +130,31 @@ class PHPUnit
 		}
 		$results = implode(",", $results);
 		$results = '['.$results.']';
-		
+
 		$results = str_replace('\n', "", $results);
 		$results = str_replace('&quot;', '"', $results);
-		
+
 		$collectedData = explode('|||', $collectedData);
-		
-		
+
+
 		$results = json_decode($results, true);
-		
+
 		// map collected data to results
 		foreach ($collectedData as $i => $data) {
-			
+
 			if(isset($results[$i])) {
 				$results[$i]['collected'] = $data;
 			}
 		}
-		
+
 		$out = array();
-		
+
 		// Maybe do something later with first result (like header)
 
 		$header = array_shift($results);
 		$started = null;
 		$test = array();
-				
+
 		foreach ($results as $i => $event) {
 			if($event['event'] == 'suiteStart') {
 				if(isset($started)) {
@@ -167,11 +172,11 @@ class PHPUnit
 						<div class="more '.$suite_display.'">
 							<hr class = "big" />
 						';
-				
+
 					$right = '</div></div>';
-				
+
 					$out[] = $left.implode('', $test).$right;
-				}	
+				}
 				$started = $event;
 				$test = array();
 				$suite_success = 'success';
@@ -191,7 +196,7 @@ class PHPUnit
 						$status = 'failure';
 						break;
 				}
-				
+
 				if($status == 'incomplete' && $suite_success != 'failure' && $suite_success != 'skipped') {
 					$suite_success = 'incomplete';
 				} if($status == 'skipped' && $suite_success != 'failure') {
@@ -199,7 +204,7 @@ class PHPUnit
 				} elseif($status == 'failure') {
 					$suite_success = 'failure';
 				}
-				
+
 				$temp = explode('::', $event['test']);
 				$name = end($temp);
 				$message = (isset($event['message'])) ? $event['message'] : '';
@@ -207,7 +212,7 @@ class PHPUnit
 				$display = ($status == 'fail') ? 'show' : 'hide';
 				$trace = (isset($event['trace'])) ? $event['trace'] : '';
 				$show_trace = ($trace) ? 'show' : 'hide';
-				
+
 				if($message) {
 					$message_pieces = explode('Failed', $message);
 					$first = array_shift($message_pieces);
@@ -220,7 +225,7 @@ class PHPUnit
 						$message = $first;
 					}
 				}
-				
+
 				if($trace) {
 					$newTrace = array();
 					foreach ($trace as $j => $arr) {
@@ -231,7 +236,7 @@ class PHPUnit
 							$newTrace[] = $arr;
 						}
 					}
-					
+
 					if(!empty($newTrace)) {
 						ob_start();
 						print_R($newTrace);
@@ -241,25 +246,25 @@ class PHPUnit
 						$trace = '';
 						$show_trace = 'hide';
 					}
-					
+
 				}
-				
+
 				$variables = (isset($event['collected'])) ? $event['collected'] : '';
 				$show_variables = ($variables) ? 'show' : 'hide';
-				
+
 				$test[] = '<div class="test '.$status.'">
 					<div class="light rounded"></div>
 					<div class="name">'.$name.'</div>
 					<div class="stats">'.$message.'</div>';
-									
+
 				if($variables && $status == 'failure') {
 					$test[] = '<div class="expand button">-</div>';
 					$display = 'show';
 				}
 				elseif ($variables || $trace) {
 					$test[] = '<div class="expand button">'.$expand.'</div>';
-				}	
-				
+				}
+
 				$test[] = '<div class="more test '.$display.'">
 						<div class="variables rounded '.$show_variables.'">
 							<pre>'.trim($variables).'</pre>
@@ -270,19 +275,19 @@ class PHPUnit
 					</div>
 				</div>
 				';
-				
+
 				if(isset($results[$i+1]) && $results[$i+1]['event'] != 'suiteStart') {
 					$test[] = '<hr class = "small" />';
 				}
-			}	
-				
+			}
+
 		}
-		
+
 		if(isset($started)) {
 			// Close out last case
 			$suite_expand = ($suite_success == 'failure') ? '-' : '+';
 			$suite_display = ($suite_success == 'failure') ? 'show' : 'hide';
-			
+
 			$left = '<div class="box rounded">
 				<div class="testsuite '.$suite_success.'">
 					<div class="light rounded"></div>
@@ -293,44 +298,44 @@ class PHPUnit
 				<div class="more '.$suite_display.'">
 					<hr class = "big" />
 				';
-			
+
 			$right = '</div></div>';
-			
+
 			$out[] = $left.implode('', $test).$right;
-			
+
 		}
-		
+
 		// Deal with last test case
-		
+
 		$out = implode('', $out);
-		
+
 		// Fill in unknowns
-		
-		
+
+
 		// exit(0);
-		
+
 		return $out;
 	}
-	
+
 	private static function push($old, $new, $subject) {
 		// echo $old;echo " | ";echo htmlspecialchars($new);echo "<br/>";
 		$pos = strpos($subject,$old);
-		
+
 		if ($pos !== false)
-		    return substr_replace($subject,$new,$pos,strlen($old));
+			 return substr_replace($subject,$new,$pos,strlen($old));
 		else
 			throw new Exception("Cannot find tag to replace (old: $old, new: ".htmlspecialchars($new).")", 1);
 	}
-	
+
 	private static function pull($str, $start = '{', $end = '}') {
 		$tags = array();
 		$nest = -1;
 		$start_mark = 0;
-		
-		for ($i=0; $i < strlen($str); $i++) { 
+
+		for ($i=0; $i < strlen($str); $i++) {
 			$start_substr = substr($str, $i, strlen($start));
 			$end_substr = substr($str, $i, strlen($end));
-			
+
 			if($start_substr == $start) {
 				$nest++;
 				if($nest == 0) {
@@ -346,16 +351,16 @@ class PHPUnit
 				$nest--;
 			}
 		}
-		
+
 		if($nest != -1) {
 			throw new Exception("Unable to parse - probably forgot a curly!", 1);
 		}
-		
+
 		return $tags;
 	}
-	
+
 }
-// 
+//
 // $phpunit = new PHPUnit('../ScarletFinal/tests');
 // echo $phpunit;
 

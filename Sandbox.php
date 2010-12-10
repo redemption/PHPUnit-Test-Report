3b6cb35a5b1fbe6a7fb0e8960aa3750a51669c22
redemption <bluespire@gmail.com>
2010-12-09 20:04:33 -0800

    Fix no output occuring unless Test files have fatal errors.  Seems like content was being passed to toHTML even though it is already HTML, causing content to disappear.

--- Sandbox.php
+++ Sandbox.php
@@ -8,50 +8,52 @@ register_shutdown_function('check_for_errors');
 function check_for_errors() {
 	function toHTML($type, $e) {
 		$left = '<div class="box rounded error">';
-		
+
 		$out = '<div class="light rounded">!</div>
 				<div class="message">'.$type.': '.$e['message'].'</div>
 				<div class="line">'.$e['line'].'</div>
 				<div class="file">'.$e['file'].'</div>';
 		$right = '</div>';
-		
-		
-		
+
+
+
 		return $left.$out.$right;
 	}
 
 	$results_so_far = '';
-	if(@ob_get_contents()) {
+	if(stristr(@ob_get_contents(),"<body>")) { //this is already HTML
+		//do nothing for now.  In the future we may change behaviour
+	} elseif (@ob_get_contents()) { //this is json output, process it.
 		$results_so_far = @ob_get_contents();
 		@ob_clean();
-		
+
 		echo PHPUnit::toHTML($results_so_far);
 	}
-		
 
-    if( false === is_null($aError = error_get_last()) ) {
 
-        switch($aError['type']) {
-            case E_NOTICE:
+	 if( false === is_null($aError = error_get_last()) ) {
+
+		  switch($aError['type']) {
+				case E_NOTICE:
 				echo toHTML("Notice", $aError);
 				break;
 			case E_WARNING:
 				echo toHTML("Warning", $aError);
 				break;
-            case E_ERROR:
+				case E_ERROR:
 				echo toHTML("Error", $aError);
 				break;
-            case E_PARSE:
+				case E_PARSE:
 				echo toHTML("Parse", $aError);
-                break;
+					 break;
 			default:
 				echo toHTML("Unknown", $aError);
 				echo "<br/>";
 				break;
-        }
+		  }
 
-		
-    }
+
+	 }
 
 	if($results_so_far) {
 		include('Main/footer.php');
